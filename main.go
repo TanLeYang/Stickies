74d0edba7705d33d600876af5e9168a9105f3c96
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2022-12-29 16:40:38 +0800

    Basic struct and update handling flow

--- main.go
+++ main.go
@@ -1,11 +1,68 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+type StickiesBot struct {
+	tgBotAPI *tgbotapi.BotAPI
+}
+
+func NewStickiesBot(tgBotAPI *tgbotapi.BotAPI) *StickiesBot {
+	return &StickiesBot{
+		tgBotAPI,
+	}
+}
+
+func (sb *StickiesBot) Start() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := sb.tgBotAPI.GetUpdatesChan(u)
+
+	go sb.receiveUpdates(ctx, updates)
+
+	log.Println("Start listening for updates, press enter to stop")
+
+	// Wait for a newline symbol, then cancel handling updates
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	cancel()
+}
+
+func (sb *StickiesBot) receiveUpdates(ctx context.Context, updates tgbotapi.UpdatesChannel) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case update := <-updates:
+			sb.handleUpdate(update)
+		}
+	}
+}
+
+func (sb *StickiesBot) handleUpdate(update tgbotapi.Update) {
+	switch {
+	// Handle messages
+	case update.Message != nil:
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		sb.tgBotAPI.Send(msg)
+		break
+
+	// Handle button clicks
+	case update.CallbackQuery != nil:
+		log.Fatalf("Handling button clicks not implemented yet")
+	}
+}
 
 func main() {
 	tgBotToken := os.Getenv("TG_BOT_TOKEN")
@@ -16,21 +73,8 @@ func main() {
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Telegram Bot API Authorized on account %s", bot.Self.UserName)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		}
-	}
+	stickiesBot := NewStickiesBot(bot)
+	stickiesBot.Start()
 }
