03cecd98b9e790e35d18b3a67e2a91c627103efe
TanLeYang <48142301+TanLeYang@users.noreply.github.com>
2023-01-06 11:49:57 +0800

    Remove unnecessary files

--- command/command.go
+++ command/command.go
@@ -6,3 +6,5 @@ type Command interface {
 	Start(*tgbotapi.Message)
 	Handle(*tgbotapi.Message)
 }
+
+type CommandStage string
--- command/stage.go
+++
@@ -1,9 +0,0 @@
-package command
-
-type CommandStage string
-
-// Shared stages
-const (
-	Start CommandStage = "START"
-	End   CommandStage = "END"
-)
